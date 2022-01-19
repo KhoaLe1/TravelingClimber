8273b167ca58ff1e4f8e700af526b9605ebff22a
Khoa Le <khoale177@gmail.com>
2022-01-18 17:08:53 -0800

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import { AutoComplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core';
-import SearchIcon from '@material-ui/icons';
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './styles';
 const Header = () => {
+    const classes = useStyles();
     return (
         <AppBar positiion = "static">
-            <Toolbar className ={classes.toolbar}>
+            <Toolbar className = {classes.toolbar}>
                 <Typography vairant = "h5" className = {classes.title}>
                     Travel Advisor
-
                 </Typography>
                 <Box display = "flex">
-                <Typography vairant = "h6" className = {classes.title}>
+                    <Typography variant = "h6" className = {classes.title}>
                     Explore new climbing places
-
-                </Typography>
-                <AutoComplete>
-                    <div className={classes.search}>
-                        <div className = {classes.searchIcon}>
-                            <SearchIcon />
+                    </Typography>
+                    {/*<AutoComplete>*/}
+                        <div className={classes.search}>
+                            <div className = {classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase placeholder="Search..." 
+                            classes = {{ root: classes.inputRoot, input: classes.inputInput}}/>
                         </div>
-                        <InputBase placebolder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput}}/>
-                    </div>
-                </AutoComplete>
+                {/*</AutoComplete>*/}
                 </Box>
             </Toolbar>
         </AppBar>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
+import { GoogleMap } from '@react-google-maps/api';
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const coordinates = { lat: 39.71738, lng: -96.96472 };
     return (
-        <h1>Map</h1>
+        <div className = {classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys = {{ key: 'AIzaSyAc17-lDbQbtYXKcvyANqYth_esPCbjk3g'}}
+                defaultCenter = {coordinates}
+                center = {coordinates}
+                defaultZoom = {4}
+                margin = {[50,50,50,50]}
+                options = {''}
+                onChange = {''}
+                onChildClick = {''}
+            >
+
+            </GoogleMapReact>
+        </div>
         );
 }
 
